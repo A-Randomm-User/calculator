1a816c9acabd633e4ee8423b27bfcd838933478f
A Randomm User <lesjsle@gmail.com>
2026-07-09 17:09:06 +0800

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Aaron on 09/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
