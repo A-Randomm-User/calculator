74306db2bd57aad7b695e8c5142e71071221d387
A Randomm User <lesjsle@gmail.com>
2026-07-29 22:59:44 +0800

    Build 13

---
+++ calculator/Convert/RadixView.swift
@@ -0,0 +1,18 @@
+//
+//  RadixView.swift
+//  calculator
+//
+//  Created by Aaron on 29/07/2026.
+//
+
+import SwiftUI
+
+struct RadixView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RadixView()
+}
